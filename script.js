ba23f4ea9f6ee23c822047ec82fa1971a0d8bf59
roofclimb <s10243726@connect.np.edu.sg>
2023-01-21 12:09:32 +0800

    draft

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-var navbar = document.getElementById("navbar")
-var menubar = document.getElementById("menu")
 
-window.onscroll = function(){
-    if(window.pageYOffset >= menu.offsetTop){
-        navbar.classList.add("sticky");
-    }
-    else{
-        navbar.classList.remove("sticky");
-    }
-}
 
