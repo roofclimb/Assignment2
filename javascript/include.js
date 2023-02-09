74528cedb27f99873abf9ddf8012dc4a71f88c8d
roofclimb <s10243726@connect.np.edu.sg>
2023-02-09 20:38:42 +0800

    slider update

--- javascript/include.js
+++ javascript/include.js
@@ -25,3 +25,20 @@ function includeHTML() {
       }
     }
   }
+
+  var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
